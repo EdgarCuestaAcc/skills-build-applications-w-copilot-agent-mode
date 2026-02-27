5393f14fd6b004250cbe48806b73c61b151985fc
EdgarCuestaAcc <e.cuesta.fernandez@accenture.com>
2026-02-27 17:08:08 +0000

    Step 4

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,13 @@ SECRET_KEY = 'django-insecure-gzq#1ntmoyaj@nx+qqvacz!e+m-^fz75xx7v2lq@fklj@+)cw0
 DEBUG = True
 
 
-# Permitir todos los hosts
-ALLOWED_HOSTS = ['*']
+# Permitir localhost y el dominio de Codespaces
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
+    ALLOWED_HOSTS.append(CODESPACE_NAME)
 
 
 # Application definition
